181865bd7b20e5183c97109ad29e764f2c73ad33
lucas <luben93@gmail.com>
2025-09-09 00:46:57 +0200

    Apply suggestion from @luben93

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.1
+// swift-tools-version: 6.2
 //===----------------------------------------------------------------------===//
 // Copyright © 2025 Apple Inc. and the container project authors. All rights reserved.
 //
